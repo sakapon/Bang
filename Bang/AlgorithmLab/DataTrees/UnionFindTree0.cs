b626a6a3007de51e0640f0281186bacbed913ce4
Keiho Sakapon <git@saka-pon.net>
2020-11-19 16:32:24 +0900

    Add basic Union-Find tree

---
+++ Bang/AlgorithmLab/DataTrees/UnionFindTree0.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmLab.DataTrees
+{
+	// 基本的な実装です。
+	public class UnionFindTree0
+	{
+		readonly int[] p;
+		public UnionFindTree0(int n) => p = Enumerable.Range(0, n).ToArray();
+		public int GetRoot(int x) => p[x] == x ? x : p[x] = GetRoot(p[x]);
+		public bool AreUnited(int x, int y) => GetRoot(x) == GetRoot(y);
+		public void Unite(int x, int y) { if (!AreUnited(x, y)) p[p[y]] = p[x]; }
+	}
+}

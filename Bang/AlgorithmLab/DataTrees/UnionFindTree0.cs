21a7697e877df4cf18b7ea2cc0b1cb3b708e5f9f
Keiho Sakapon <git@saka-pon.net>
2020-11-20 13:50:45 +0900

    Update Quick Find

--- Bang/AlgorithmLab/DataTrees/UnionFindTree0.cs
+++ Bang/AlgorithmLab/DataTrees/UnionFindTree0.cs
@@ -7,7 +7,7 @@ namespace AlgorithmLab.DataTrees
 	// 基本的な実装です。
 	public class UnionFindTree0
 	{
-		readonly int[] p;
+		int[] p;
 		public UnionFindTree0(int n) => p = Enumerable.Range(0, n).ToArray();
 		public int GetRoot(int x) => p[x] == x ? x : p[x] = GetRoot(p[x]);
 		public bool AreUnited(int x, int y) => GetRoot(x) == GetRoot(y);
@@ -16,22 +16,27 @@ namespace AlgorithmLab.DataTrees
 
 	public class QuickFind
 	{
-		readonly int[] p;
-		readonly List<int>[] g;
+		int[] p;
+		List<int>[] g;
+		public int GroupsCount { get; private set; }
 		public QuickFind(int n)
 		{
 			p = Enumerable.Range(0, n).ToArray();
 			g = Array.ConvertAll(p, x => new List<int> { x });
+			GroupsCount = n;
 		}
 		public int GetRoot(int x) => p[x];
+		public int GetSize(int x) => g[p[x]].Count;
+		public List<int> GetGroup(int x) => g[p[x]];
 		public bool AreUnited(int x, int y) => p[x] == p[y];
-		public void Unite(int x, int y)
+		public bool Unite(int x, int y)
 		{
-			(x, y) = (p[x], p[y]);
-			if (x == y) return;
+			if ((x = p[x]) == (y = p[y])) return false;
 			if (g[x].Count < g[y].Count) (x, y) = (y, x);
 			foreach (var z in g[y]) p[z] = x;
 			g[x].AddRange(g[y]);
+			--GroupsCount;
+			return true;
 		}
 	}
 }
---
+++ Bang/OnlineTest/DataTrees/UnionFindTreeTest2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees;
+
+namespace OnlineTest.DataTrees
+{
+	// Test: https://atcoder.jp/contests/abl/tasks/abl_c
+	class UnionFindTreeTest2
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main()
+		{
+			var h = Read();
+			int n = h[0], m = h[1];
+			var es = Array.ConvertAll(new bool[m], _ => Read());
+
+			var uf = new QuickFind(n + 1);
+			foreach (var e in es)
+				uf.Unite(e[0], e[1]);
+			Console.WriteLine(uf.GroupsCount - 2);
+		}
+	}
+}

61dc0c0dfecfc1068aebe3175d433bdf70093208
Keiho Sakapon <git@saka-pon.net>
2020-11-20 11:17:19 +0900

    Add class for Quick Find

--- Bang/AlgorithmLab/DataTrees/UnionFindTree0.cs
+++ Bang/AlgorithmLab/DataTrees/UnionFindTree0.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AlgorithmLab.DataTrees
@@ -11,5 +12,26 @@ namespace AlgorithmLab.DataTrees
 		public int GetRoot(int x) => p[x] == x ? x : p[x] = GetRoot(p[x]);
 		public bool AreUnited(int x, int y) => GetRoot(x) == GetRoot(y);
 		public void Unite(int x, int y) { if (!AreUnited(x, y)) p[p[y]] = p[x]; }
+	}
+
+	public class QuickFind
+	{
+		readonly int[] p;
+		readonly List<int>[] g;
+		public QuickFind(int n)
+		{
+			p = Enumerable.Range(0, n).ToArray();
+			g = Array.ConvertAll(p, x => new List<int> { x });
+		}
+		public int GetRoot(int x) => p[x];
+		public bool AreUnited(int x, int y) => p[x] == p[y];
+		public void Unite(int x, int y)
+		{
+			(x, y) = (p[x], p[y]);
+			if (x == y) return;
+			if (g[x].Count < g[y].Count) (x, y) = (y, x);
+			foreach (var z in g[y]) p[z] = x;
+			g[x].AddRange(g[y]);
+		}
 	}
 }
---
+++ Bang/OnlineTest/DataTrees/UnionFindTreeTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees;
+
+namespace OnlineTest.DataTrees
+{
+	// Test: https://atcoder.jp/contests/practice2/tasks/practice2_a
+	class UnionFindTreeTest1
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main()
+		{
+			Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+			var h = Read();
+			int n = h[0], qc = h[1];
+			var qs = Array.ConvertAll(new bool[qc], _ => Read());
+
+			var uf = new UnionFindTree0(n);
+
+			foreach (var q in qs)
+				if (q[0] == 0)
+					uf.Unite(q[1], q[2]);
+				else
+					Console.WriteLine(uf.AreUnited(q[1], q[2]) ? 1 : 0);
+			Console.Out.Flush();
+		}
+	}
+}

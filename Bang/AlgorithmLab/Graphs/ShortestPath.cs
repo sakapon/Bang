a212fd04c892a27136a530d308a2d1d8befdee95
Keiho Sakapon <git@saka-pon.net>
2020-12-15 15:17:43 +0900

    Add BFS functions for T-type vertexes

--- Bang/AlgorithmLab/Graphs/ShortestPath.cs
+++ Bang/AlgorithmLab/Graphs/ShortestPath.cs
@@ -5,10 +5,38 @@ namespace AlgorithmLab.Graphs
 {
 	public static class ShortestPath
 	{
+		public static Func<T, long> Bfs<T>(int vertexesCount, Func<T, int> toId, Func<int, T> fromId, Func<T, IEnumerable<T>> getNextVertexes, T startVertex, T endVertex)
+		{
+			IEnumerable<int> toNextIds(int v)
+			{
+				foreach (var nv in getNextVertexes(fromId(v)))
+					yield return toId(nv);
+			}
+			var r = ShortestPathCore.Bfs(vertexesCount, toNextIds, toId(startVertex), toId(endVertex));
+			return v => r[toId(v)];
+		}
+
+		public static Func<T, long> Bfs<T>(int vertexesCount, Func<T, int> toId, T[][] edges, bool directed, T startVertex, T endVertex)
+		{
+			var r = ShortestPathCore.Bfs(vertexesCount, Array.ConvertAll(edges, e => new[] { toId(e[0]), toId(e[1]) }), directed, toId(startVertex), toId(endVertex));
+			return v => r[toId(v)];
+		}
 	}
 
 	public static class ShortestPathCore
 	{
+		/// <summary>
+		/// 幅優先探索により、始点からの最短経路を求めます。
+		/// </summary>
+		/// <param name="vertexesCount">頂点の個数。これ未満の値を ID として使用できます。</param>
+		/// <param name="getNextVertexes">指定された頂点からの行先を取得するための関数。</param>
+		/// <param name="startVertexId">始点の ID。</param>
+		/// <param name="endVertexId">終点の ID。終点を指定しない場合、-1。</param>
+		/// <returns>頂点ごとの最小コスト。到達不可能の場合、<see cref="long.MaxValue"/>。</returns>
+		/// <remarks>
+		/// グラフの有向性、連結性、多重性、開閉を問いません。したがって、1-indexed でも利用できます。<br/>
+		/// 辺のコストはすべて 1 です。
+		/// </remarks>
 		public static long[] Bfs(int vertexesCount, Func<int, IEnumerable<int>> getNextVertexes, int startVertexId, int endVertexId = -1)
 		{
 			var costs = Array.ConvertAll(new bool[vertexesCount], _ => long.MaxValue);

dd2c75a3abacf2b65124d01de5ee2ecca015b05f
Keiho Sakapon <git@saka-pon.net>
2020-12-21 22:32:37 +0900

    Update code

--- Bang/AlgorithmLab/Graphs0/ShortestPath1.cs
+++ Bang/AlgorithmLab/Graphs0/ShortestPath1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace AlgorithmLab.Graphs0
 {
@@ -8,15 +7,6 @@ namespace AlgorithmLab.Graphs0
 	/// </summary>
 	public static class ShortestPath1
 	{
-		// 無向グリッド上での典型的な BFS の例
-		// ev: 終点を指定しない場合、(-1, -1) など
-		// 
-		//var r = ShortestPath.Bfs(h * w,
-		//	v => GridHelper.ToId(v, w),
-		//	id => GridHelper.FromId(id, w),
-		//	v => Array.FindAll(GridHelper.Nexts(v), v => s.GetByP(v) != '#'),
-		//	sv, ev);
-
 		public static UnweightedResult Bfs(int vertexesCount, int[][] edges, bool directed, int startVertexId, int endVertexId = -1)
 		{
 			var map = GraphConvert1.UnweightedEdgesToMap(vertexesCount, edges, directed);
@@ -31,7 +21,8 @@ namespace AlgorithmLab.Graphs0
 
 		public static WeightedResult Dijkstra(int vertexesCount, int[][] edges, bool directed, int startVertexId, int endVertexId = -1)
 		{
-			return Dijkstra(vertexesCount, Array.ConvertAll(edges, e => new Edge(e)), directed, startVertexId, endVertexId);
+			var map = GraphConvert1.WeightedEdgesToMap(vertexesCount, edges, directed);
+			return ShortestPathCore1.Dijkstra(vertexesCount, v => map[v].ToArray(), startVertexId, endVertexId);
 		}
 	}
 }

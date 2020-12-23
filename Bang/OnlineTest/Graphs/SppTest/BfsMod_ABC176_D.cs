ca192554a0cd5c03e78045429d9f7b243ef3b1aa
Keiho Sakapon <git@saka-pon.net>
2020-12-23 22:17:27 +0900

    Use extended BFS

--- Bang/OnlineTest/Graphs/SppTest/BfsMod_ABC176_D.cs
+++ Bang/OnlineTest/Graphs/SppTest/BfsMod_ABC176_D.cs
@@ -5,7 +5,7 @@ using AlgorithmLab.Graphs;
 namespace OnlineTest.Graphs.SppTest
 {
 	// Test: https://atcoder.jp/contests/abc176/tasks/abc176_d
-	class Dijkstra_ABC176_D
+	class BfsMod_ABC176_D
 	{
 		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 		static (int i, int j) Read2() { var a = Read(); return (a[0], a[1]); }
@@ -17,28 +17,28 @@ namespace OnlineTest.Graphs.SppTest
 			var ev = GraphConsole.ReadPoint() + (1, 1);
 			var s = GraphConsole.ReadEnclosedGrid(ref h, ref w, delta: 2);
 
-			// WeightedEdge<T>[] を静的に構築する方法では TLE。
+			// Dijkstra でも AC。ただし、WeightedEdge<T>[] を静的に構築する方法では TLE。
 			var r = ShortestPath.WithGrid(h, w)
 				.WithWeighted(v =>
 				{
-					var nids = new List<WeightedEdge<Point>>();
+					var nvs = new List<WeightedEdge<Point>>();
 
 					foreach (var nv in v.Nexts())
 					{
 						if (s.GetValue(nv) == '#') continue;
-						nids.Add((v, nv, 0));
+						nvs.Add(new WeightedEdge<Point>(v, nv, 0));
 					}
 
 					for (int i = -2; i <= 2; i++)
 						for (int j = -2; j <= 2; j++)
 						{
-							var nv = v + (i, j);
+							var nv = v + new Point(i, j);
 							if (s.GetValue(nv) == '#') continue;
-							nids.Add((v, nv, 1));
+							nvs.Add(new WeightedEdge<Point>(v, nv, 1));
 						}
-					return nids.ToArray();
+					return nvs.ToArray();
 				})
-				.Dijkstra(sv, ev);
+				.BfsMod(2, sv, ev);
 
 			Console.WriteLine(r.IsConnected(ev) ? r[ev] : -1);
 		}

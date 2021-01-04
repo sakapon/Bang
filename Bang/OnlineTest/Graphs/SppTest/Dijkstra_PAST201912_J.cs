c5f03c89f6668fad2e2e758f37a6f5f28049dd42
Keiho Sakapon <git@saka-pon.net>
2021-01-04 16:07:10 +0900

    Add test

---
+++ Bang/OnlineTest/Graphs/SppTest/Dijkstra_PAST201912_J.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Graphs;
+
+namespace OnlineTest.Graphs.SppTest
+{
+	// Test: https://atcoder.jp/contests/past201912-open/tasks/past201912_j
+	class Dijkstra_PAST201912_J
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main()
+		{
+			var (h, w) = Read2();
+			var a = GraphConsole.ReadGridAsInt(h);
+
+			WeightedMapSpp<Point> Dijkstra(Point sv) => ShortestPath.ForGrid(h, w)
+				.ForWeightedMap(v => Array.ConvertAll(Array.FindAll(v.Nexts(), nv => nv.IsInRange(h, w)), nv => new Edge<Point>(v, nv, a.GetValue(nv))))
+				.Dijkstra(sv, (-1, -1));
+
+			var r1 = Dijkstra((h - 1, 0));
+			var r2 = Dijkstra((h - 1, w - 1));
+			var r3 = Dijkstra((0, w - 1));
+
+			var m = long.MaxValue;
+			for (int i = 0; i < h; i++)
+				for (int j = 0; j < w; j++)
+				{
+					var v = (i, j);
+					m = Math.Min(m, r1[v] + r2[v] + r3[v] - 2 * a.GetValue(v));
+				}
+			Console.WriteLine(m);
+		}
+	}
+}
---
+++ Bang/OnlineTest/Graphs/SppTest/Dijkstra_PAST202010_J.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Graphs;
+
+namespace OnlineTest.Graphs.SppTest
+{
+	// Test: https://atcoder.jp/contests/past202010-open/tasks/past202010_j
+	class Dijkstra_PAST202010_J
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main()
+		{
+			var (n, m) = Read2();
+			var (xab, xac, xbc) = Read3();
+			var s = Console.ReadLine();
+			var es = GraphConsole.ReadEdges(m);
+
+			var (vab, vac, vbc) = (n + 1, n + 2, n + 3);
+			var (vba, vca, vcb) = (n + 4, n + 5, n + 6);
+
+			var spp = ShortestPath.ForInt(n + 7).ForWeightedMap();
+			foreach (var e in es)
+			{
+				spp.AddEdge(e, false);
+			}
+
+			for (int v = 1; v <= n; v++)
+			{
+				if (s[v - 1] == 'A')
+				{
+					spp.AddEdge(v, vab, xab, true);
+					spp.AddEdge(v, vac, xac, true);
+					spp.AddEdge(vba, v, 0, true);
+					spp.AddEdge(vca, v, 0, true);
+				}
+				else if (s[v - 1] == 'B')
+				{
+					spp.AddEdge(v, vba, xab, true);
+					spp.AddEdge(v, vbc, xbc, true);
+					spp.AddEdge(vab, v, 0, true);
+					spp.AddEdge(vcb, v, 0, true);
+				}
+				else
+				{
+					spp.AddEdge(v, vcb, xbc, true);
+					spp.AddEdge(v, vca, xac, true);
+					spp.AddEdge(vbc, v, 0, true);
+					spp.AddEdge(vac, v, 0, true);
+				}
+			}
+			spp.Dijkstra(1, n);
+			Console.WriteLine(spp[n]);
+		}
+	}
+}

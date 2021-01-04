4ffac04fbe7fce628388e87c0f556a8dc63d0bc1
Keiho Sakapon <git@saka-pon.net>
2021-01-04 14:19:12 +0900

    Add test

---
+++ Bang/OnlineTest/Graphs/SppTest/Bfs_PAST202012_H.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Graphs;
+
+namespace OnlineTest.Graphs.SppTest
+{
+	// Test: https://atcoder.jp/contests/past202012-open/tasks/past202012_h
+	class Bfs_PAST202012_H
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main()
+		{
+			var (h, w) = Read2();
+			Point sv = Read2();
+			var s = GraphConsole.ReadEnclosedGrid(ref h, ref w);
+
+			const string arrows = "v^><";
+			var spp = ShortestPath.ForGrid(h, w)
+				.ForUnweightedMap(v =>
+				{
+					var nvs = new List<Point>();
+					var nva = v.Nexts();
+
+					for (int k = 0; k < 4; k++)
+					{
+						var (i, j) = nva[k];
+						if (s[i][j] == '.' || s[i][j] == arrows[k]) nvs.Add(nva[k]);
+					}
+					return nvs.ToArray();
+				})
+				.Bfs(sv, (-1, -1));
+
+			Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+			for (int i = 1; i < h - 1; i++)
+			{
+				for (int j = 1; j < w - 1; j++)
+					Console.Write(s[i][j] == '#' ? '#' : spp.IsConnected((i, j)) ? 'o' : 'x');
+				Console.WriteLine();
+			}
+			Console.Out.Flush();
+		}
+	}
+}
---
+++ Bang/OnlineTest/Graphs/SppTest/Bfs_PAST202012_I.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Graphs;
+
+namespace OnlineTest.Graphs.SppTest
+{
+	// Test: https://atcoder.jp/contests/past202012-open/tasks/past202012_i
+	class Bfs_PAST202012_I
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main()
+		{
+			var (n, m, k) = Read3();
+			var h = Read();
+			var c = Read();
+			var es = Array.ConvertAll(new bool[m], _ => Read());
+
+			var spp = ShortestPath.ForInt(n + 1).ForUnweightedMap();
+			foreach (var v in c)
+			{
+				spp.AddEdge(0, v, true);
+			}
+			foreach (var e in es)
+			{
+				if (h[e[0] - 1] > h[e[1] - 1]) (e[0], e[1]) = (e[1], e[0]);
+				spp.AddEdge(e[0], e[1], true);
+			}
+			spp.Bfs(0, -1);
+
+			Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+			for (int v = 1; v <= n; v++)
+				Console.WriteLine(spp.IsConnected(v) ? spp[v] - 1 : -1);
+			Console.Out.Flush();
+		}
+	}
+}

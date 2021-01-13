166269f4a355f931f321374fe449cbe2c2650899
Keiho Sakapon <git@saka-pon.net>
2021-01-13 10:45:13 +0900

    Add UnweightedMap class

--- Bang/AlgorithmLab/Graphs/Int/GraphConvert.cs
+++ Bang/AlgorithmLab/Graphs/Int/GraphConvert.cs
@@ -5,33 +5,69 @@ namespace AlgorithmLab.Graphs.Int
 {
 	public static class GraphConvert
 	{
-		public static List<int>[] UnweightedEdgesToMap(int vertexesCount, int[][] edges, bool directed)
+		public static void UnweightedEdgesToMap(List<int>[] map, Edge[] edges, bool directed)
 		{
-			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
 			foreach (var e in edges)
 			{
-				// 入力チェックは省略。
+				map[e.From].Add(e.To);
+				if (!directed) map[e.To].Add(e.From);
+			}
+		}
+
+		public static void UnweightedEdgesToMap(List<int>[] map, int[][] edges, bool directed)
+		{
+			foreach (var e in edges)
+			{
 				map[e[0]].Add(e[1]);
 				if (!directed) map[e[1]].Add(e[0]);
 			}
+		}
+
+		public static void WeightedEdgesToMap(List<Edge>[] map, Edge[] edges, bool directed)
+		{
+			foreach (var e in edges)
+			{
+				map[e.From].Add(e);
+				if (!directed) map[e.To].Add(e.Reverse());
+			}
+		}
+
+		public static void WeightedEdgesToMap(List<Edge>[] map, int[][] edges, bool directed)
+		{
+			foreach (var e0 in edges)
+			{
+				Edge e = e0;
+				map[e.From].Add(e);
+				if (!directed) map[e.To].Add(e.Reverse());
+			}
+		}
+
+		public static List<int>[] UnweightedEdgesToMap(int vertexesCount, Edge[] edges, bool directed)
+		{
+			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
+			UnweightedEdgesToMap(map, edges, directed);
+			return map;
+		}
+
+		public static List<int>[] UnweightedEdgesToMap(int vertexesCount, int[][] edges, bool directed)
+		{
+			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
+			UnweightedEdgesToMap(map, edges, directed);
 			return map;
 		}
 
 		public static List<Edge>[] WeightedEdgesToMap(int vertexesCount, Edge[] edges, bool directed)
 		{
 			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<Edge>());
-			foreach (var e in edges)
-			{
-				// 入力チェックは省略。
-				map[e.From].Add(e);
-				if (!directed) map[e.To].Add(e.Reverse());
-			}
+			WeightedEdgesToMap(map, edges, directed);
 			return map;
 		}
 
 		public static List<Edge>[] WeightedEdgesToMap(int vertexesCount, int[][] edges, bool directed)
 		{
-			return WeightedEdgesToMap(vertexesCount, Array.ConvertAll(edges, e => (Edge)e), directed);
+			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<Edge>());
+			WeightedEdgesToMap(map, edges, directed);
+			return map;
 		}
 	}
 }
---
+++ Bang/AlgorithmLab/Graphs/Int/UnweightedMap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLab.Graphs.Int
+{
+	public class UnweightedMap
+	{
+		List<int>[] map;
+		public List<int>[] RawMap => map;
+		public int[] this[int vertex] => map[vertex].ToArray();
+
+		public UnweightedMap(List<int>[] map) { this.map = map; }
+
+		public UnweightedMap(int vertexesCount)
+		{
+			map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
+		}
+
+		public UnweightedMap(int vertexesCount, Edge[] edges, bool directed) : this(vertexesCount)
+		{
+			GraphConvert.UnweightedEdgesToMap(map, edges, directed);
+		}
+
+		public UnweightedMap(int vertexesCount, int[][] edges, bool directed) : this(vertexesCount)
+		{
+			GraphConvert.UnweightedEdgesToMap(map, edges, directed);
+		}
+
+		public void AddEdge(Edge edge, bool directed)
+		{
+			map[edge.From].Add(edge.To);
+			if (!directed) map[edge.To].Add(edge.From);
+		}
+
+		public void AddEdge(int from, int to, bool directed)
+		{
+			map[from].Add(to);
+			if (!directed) map[to].Add(from);
+		}
+
+		public void AddEdges(Edge[] edges, bool directed)
+		{
+			GraphConvert.UnweightedEdgesToMap(map, edges, directed);
+		}
+
+		public void AddEdges(int[][] edges, bool directed)
+		{
+			GraphConvert.UnweightedEdgesToMap(map, edges, directed);
+		}
+	}
+}

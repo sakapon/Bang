2b4b140822d85eb70a83ccdd4f059b30b131079a
Keiho Sakapon <git@saka-pon.net>
2021-01-14 17:28:22 +0900

    Move test

--- Bang/OnlineTest/Graphs/Int/SppTest/Dijkstra_CFR020_C.cs
+++ Bang/OnlineTest/Graphs/Int/SppTest/Dijkstra_CFR020_C.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using AlgorithmLab.DataTrees;
-using AlgorithmLab.Graphs0;
+using AlgorithmLab.Graphs.Int;
 
-namespace OnlineTest.Graphs0.Spp0Test
+namespace OnlineTest.Graphs.Int.SppTest
 {
 	// Test: https://codeforces.com/contest/20/problem/C
-	// https://codeforces.com/contest/20/submission/101973330
-	class Dijkstra_CFR020_C1
+	class Dijkstra_CFR020_C
 	{
 		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-		static Edge[] ReadEdges(int count) => Array.ConvertAll(new bool[count], _ => new Edge(Read()));
 		static void Main()
 		{
 			var (n, m) = Read2();
-			var es = ReadEdges(m);
+			var map = GraphConsole.ReadWeightedMap(n + 1, m, false);
 
-			var r = ShortestPath1.Dijkstra(n + 1, es, false, 1, n);
+			var r = map.Dijkstra(1, n);
 			if (!r.IsConnected(n)) { Console.WriteLine(-1); return; }
 
 			var path = r.GetPathVertexes(n);
--- Bang/OnlineTest/Graphs/Int/SppTest/Dijkstra_YLC_SPP.cs
+++ Bang/OnlineTest/Graphs/Int/SppTest/Dijkstra_YLC_SPP.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using AlgorithmLab.DataTrees;
-using AlgorithmLab.Graphs0;
+using AlgorithmLab.Graphs.Int;
 
-namespace OnlineTest.Graphs0.Spp0Test
+namespace OnlineTest.Graphs.Int.SppTest
 {
 	// Test: https://judge.yosupo.jp/problem/shortest_path
-	// https://judge.yosupo.jp/submission/33187
-	class Dijkstra_YLC_SPP1
+	class Dijkstra_YLC_SPP
 	{
 		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-		static Edge[] ReadEdges(int count) => Array.ConvertAll(new bool[count], _ => new Edge(Read()));
 		static void Main()
 		{
 			var h = Read();
 			int n = h[0], m = h[1], s = h[2], t = h[3];
-			var es = ReadEdges(m);
+			var map = GraphConsole.ReadWeightedMap(n, m, true);
 
-			var r = ShortestPath1.Dijkstra(n, es, true, s, t);
+			var r = map.Dijkstra(s, t);
 			if (!r.IsConnected(t)) { Console.WriteLine(-1); return; }
 
 			var path = r.GetPathEdges(t);

48567f3cfb84397c8c562d1f6759fd3ee9e51ebf
Keiho Sakapon <git@saka-pon.net>
2021-01-14 09:32:43 +0900

    Add WeightedMap class

--- Bang/AlgorithmLab/SingleFiles/Graphs.Spp.Int.cs
+++ Bang/AlgorithmLab/SingleFiles/Graphs.Spp.Int.cs
@@ -22,6 +22,17 @@ namespace AlgorithmLab.Graphs.Spp.Int
 			}
 			return map;
 		}
+
+		public static WeightedMap ReadWeightedMap(int vertexesCount, int edgesCount, bool directed)
+		{
+			var map = new WeightedMap(vertexesCount);
+			for (int i = 0; i < edgesCount; ++i)
+			{
+				var e = Read();
+				map.AddEdge(e[0], e[1], e[2], directed);
+			}
+			return map;
+		}
 	}
 
 	public static class GraphConvert
@@ -195,45 +206,31 @@ namespace AlgorithmLab.Graphs.Spp.Int
 		public Edge Reverse() => new Edge(To, From, Cost);
 	}
 
-	public class UnweightedResult
+	public class CostResult
 	{
+		protected const int InvalidVertex = -1;
+
 		public long[] RawCosts { get; }
-		public int[] RawInVertexes { get; }
+		public CostResult(long[] costs) { RawCosts = costs; }
+
 		public long this[int vertex] => RawCosts[vertex];
 		public bool IsConnected(int vertex) => RawCosts[vertex] != long.MaxValue;
+		public long GetCost(int vertex, long invalid = -1) => IsConnected(vertex) ? RawCosts[vertex] : invalid;
+	}
 
-		public UnweightedResult(long[] costs, int[] inVertexes)
+	public class UnweightedResult : CostResult
+	{
+		public int[] RawInVertexes { get; }
+
+		public UnweightedResult(long[] costs, int[] inVertexes) : base(costs)
 		{
-			RawCosts = costs;
 			RawInVertexes = inVertexes;
 		}
 
 		public int[] GetPathVertexes(int endVertex)
 		{
 			var path = new Stack<int>();
-			for (var v = endVertex; v != -1; v = RawInVertexes[v])
-				path.Push(v);
-			return path.ToArray();
-		}
-	}
-
-	public class WeightedResult
-	{
-		public long[] RawCosts { get; }
-		public Edge[] RawInEdges { get; }
-		public long this[int vertex] => RawCosts[vertex];
-		public bool IsConnected(int vertex) => RawCosts[vertex] != long.MaxValue;
-
-		public WeightedResult(long[] costs, Edge[] inEdges)
-		{
-			RawCosts = costs;
-			RawInEdges = inEdges;
-		}
-
-		public int[] GetPathVertexes(int endVertex)
-		{
-			var path = new Stack<int>();
-			for (var v = endVertex; v != -1; v = RawInEdges[v].From)
+			for (var v = endVertex; v != InvalidVertex; v = RawInVertexes[v])
 				path.Push(v);
 			return path.ToArray();
 		}
@@ -241,7 +238,33 @@ namespace AlgorithmLab.Graphs.Spp.Int
 		public Edge[] GetPathEdges(int endVertex)
 		{
 			var path = new Stack<Edge>();
-			for (var e = RawInEdges[endVertex]; e.From != -1; e = RawInEdges[e.From])
+			for (var v = endVertex; RawInVertexes[v] != InvalidVertex; v = RawInVertexes[v])
+				path.Push(new Edge(RawInVertexes[v], v));
+			return path.ToArray();
+		}
+	}
+
+	public class WeightedResult : CostResult
+	{
+		public Edge[] RawInEdges { get; }
+
+		public WeightedResult(long[] costs, Edge[] inEdges) : base(costs)
+		{
+			RawInEdges = inEdges;
+		}
+
+		public int[] GetPathVertexes(int endVertex)
+		{
+			var path = new Stack<int>();
+			for (var v = endVertex; v != InvalidVertex; v = RawInEdges[v].From)
+				path.Push(v);
+			return path.ToArray();
+		}
+
+		public Edge[] GetPathEdges(int endVertex)
+		{
+			var path = new Stack<Edge>();
+			for (var e = RawInEdges[endVertex]; e.From != InvalidVertex; e = RawInEdges[e.From])
 				path.Push(e);
 			return path.ToArray();
 		}
@@ -301,6 +324,63 @@ namespace AlgorithmLab.Graphs.Spp.Int
 		public UnweightedResult Bfs(int startVertex, int endVertex = -1)
 		{
 			return ShortestPathCore.Bfs(VertexesCount, v => this[v], startVertex, endVertex);
+		}
+	}
+
+	public class WeightedMap
+	{
+		public int VertexesCount { get; }
+		List<Edge>[] map;
+		public List<Edge>[] RawMap => map;
+		public Edge[] this[int vertex] => map[vertex].ToArray();
+
+		public WeightedMap(int vertexesCount, List<Edge>[] map)
+		{
+			VertexesCount = vertexesCount;
+			this.map = map;
+		}
+
+		public WeightedMap(int vertexesCount)
+		{
+			VertexesCount = vertexesCount;
+			map = Array.ConvertAll(new bool[vertexesCount], _ => new List<Edge>());
+		}
+
+		public WeightedMap(int vertexesCount, Edge[] edges, bool directed) : this(vertexesCount)
+		{
+			GraphConvert.WeightedEdgesToMap(map, edges, directed);
+		}
+
+		public WeightedMap(int vertexesCount, int[][] edges, bool directed) : this(vertexesCount)
+		{
+			GraphConvert.WeightedEdgesToMap(map, edges, directed);
+		}
+
+		public void AddEdge(Edge edge, bool directed)
+		{
+			map[edge.From].Add(edge);
+			if (!directed) map[edge.To].Add(edge.Reverse());
+		}
+
+		public void AddEdge(int from, int to, long cost, bool directed)
+		{
+			map[from].Add(new Edge(from, to, cost));
+			if (!directed) map[to].Add(new Edge(to, from, cost));
+		}
+
+		public void AddEdges(Edge[] edges, bool directed)
+		{
+			GraphConvert.WeightedEdgesToMap(map, edges, directed);
+		}
+
+		public void AddEdges(int[][] edges, bool directed)
+		{
+			GraphConvert.WeightedEdgesToMap(map, edges, directed);
+		}
+
+		public WeightedResult Dijkstra(int startVertex, int endVertex = -1)
+		{
+			return ShortestPathCore.Dijkstra(VertexesCount, v => this[v], startVertex, endVertex);
 		}
 	}
 
---
+++ Bang/OnlineTest/Graphs/SppTest/Int/Dijkstra_CFR677_G.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.Graphs.Int;
+
+namespace OnlineTest.Graphs.SppTest.Int
+{
+	// Test: https://codeforces.com/contest/1433/problem/G
+	class Dijkstra_CFR677_G
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main()
+		{
+			var (n, m, k) = Read3();
+			var es = GraphConsole.ReadEdges(m);
+			var rs = GraphConsole.ReadEdges(k);
+
+			var map = new WeightedMap(n + 1, es, false);
+			var costMap = Enumerable.Range(0, n + 1).Select(sv => map.Dijkstra(sv)).ToArray();
+
+			long CostSum(Edge e) => rs.Sum(r =>
+			{
+				var (a, b) = (r.From, r.To);
+				var m = costMap[a][b];
+				m = Math.Min(m, costMap[a][e.From] + costMap[e.To][b]);
+				m = Math.Min(m, costMap[a][e.To] + costMap[e.From][b]);
+				return m;
+			});
+			Console.WriteLine(es.Min(CostSum));
+		}
+	}
+}

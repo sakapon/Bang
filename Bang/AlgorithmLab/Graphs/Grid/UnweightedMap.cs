ea0c14a93a32833901e1429d32b9a1a4ece00880
Keiho Sakapon <git@saka-pon.net>
2021-01-15 17:15:47 +0900

    Add map classes

---
+++ Bang/AlgorithmLab/Graphs/Grid/UnweightedMap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLab.Graphs.Grid
+{
+	public class UnweightedMap
+	{
+		public int Height { get; }
+		public int Width { get; }
+		GridMap<List<Point>> map;
+		public GridMap<List<Point>> RawMap => map;
+		public Point[] this[Point vertex] => map[vertex].ToArray();
+
+		public UnweightedMap(int height, int width, GridMap<List<Point>> map)
+		{
+			Height = height;
+			Width = width;
+			this.map = map;
+		}
+
+		public UnweightedMap(int height, int width)
+		{
+			Height = height;
+			Width = width;
+			map = GridMap.Create(height, width, () => new List<Point>());
+		}
+
+		public UnweightedMap(int height, int width, Edge[] edges, bool directed) : this(height, width)
+		{
+			AddEdges(edges, directed);
+		}
+
+		public void AddEdges(Edge[] edges, bool directed)
+		{
+			GraphConvert.UnweightedEdgesToMap(map, edges, directed);
+		}
+
+		public void AddEdge(Edge edge, bool directed)
+		{
+			map[edge.From].Add(edge.To);
+			if (!directed) map[edge.To].Add(edge.From);
+		}
+
+		public void AddEdge(Point from, Point to, bool directed)
+		{
+			map[from].Add(to);
+			if (!directed) map[to].Add(from);
+		}
+
+		public UnweightedResult Bfs(Point startVertex, Point endVertex)
+		{
+			return ShortestPathCore.Bfs(Height, Width, v => this[v], startVertex, endVertex);
+		}
+	}
+}
---
+++ Bang/AlgorithmLab/Graphs/Grid/WeightedMap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLab.Graphs.Grid
+{
+	public class WeightedMap
+	{
+		public int Height { get; }
+		public int Width { get; }
+		GridMap<List<Edge>> map;
+		public GridMap<List<Edge>> RawMap => map;
+		public Edge[] this[Point vertex] => map[vertex].ToArray();
+
+		public WeightedMap(int height, int width, GridMap<List<Edge>> map)
+		{
+			Height = height;
+			Width = width;
+			this.map = map;
+		}
+
+		public WeightedMap(int height, int width)
+		{
+			Height = height;
+			Width = width;
+			map = GridMap.Create(height, width, () => new List<Edge>());
+		}
+
+		public WeightedMap(int height, int width, Edge[] edges, bool directed) : this(height, width)
+		{
+			AddEdges(edges, directed);
+		}
+
+		public void AddEdges(Edge[] edges, bool directed)
+		{
+			GraphConvert.WeightedEdgesToMap(map, edges, directed);
+		}
+
+		public void AddEdge(Edge edge, bool directed)
+		{
+			map[edge.From].Add(edge);
+			if (!directed) map[edge.To].Add(edge.Reverse());
+		}
+
+		public void AddEdge(Point from, Point to, long cost, bool directed)
+		{
+			map[from].Add(new Edge(from, to, cost));
+			if (!directed) map[to].Add(new Edge(to, from, cost));
+		}
+
+		public WeightedResult Dijkstra(Point startVertex, Point endVertex)
+		{
+			return ShortestPathCore.Dijkstra(Height, Width, v => this[v], startVertex, endVertex);
+		}
+
+		public WeightedResult BfsMod(int m, Point startVertex, Point endVertex)
+		{
+			return ShortestPathCore.BfsMod(m, Height, Width, v => this[v], startVertex, endVertex);
+		}
+	}
+}

12856f1a67b0188b3fd48c885abaa6df44e81f6c
Keiho Sakapon <git@saka-pon.net>
2021-01-15 15:30:53 +0900

    Add GridMap class

--- Bang/AlgorithmLab/Graphs/Grid/DataTypes.cs
+++ Bang/AlgorithmLab/Graphs/Grid/DataTypes.cs
@@ -3,57 +3,97 @@ using System.Collections.Generic;
 
 namespace AlgorithmLab.Graphs.Grid
 {
+	public abstract class GridMap<TValue>
+	{
+		public abstract TValue this[Point key] { get; set; }
+		public abstract TValue this[int i, int j] { get; set; }
+	}
+
+	public class JaggedGridMap<TValue> : GridMap<TValue>
+	{
+		TValue[][] a;
+		public JaggedGridMap(int height, int width, TValue iv)
+		{
+			a = Array.ConvertAll(new bool[height], _ => Array.ConvertAll(new bool[width], __ => iv));
+		}
+		public JaggedGridMap(int height, int width, Func<TValue> getIV)
+		{
+			a = Array.ConvertAll(new bool[height], _ => Array.ConvertAll(new bool[width], __ => getIV()));
+		}
+		public override TValue this[Point key] { get => a[key.i][key.j]; set => a[key.i][key.j] = value; }
+		public override TValue this[int i, int j] { get => a[i][j]; set => a[i][j] = value; }
+	}
+
+	public class RectGridMap<TValue> : GridMap<TValue>
+	{
+		TValue[,] a;
+		public RectGridMap(int height, int width, TValue iv)
+		{
+			a = new TValue[height, width];
+			for (int i = 0; i < height; ++i)
+				for (int j = 0; j < width; ++j)
+					a[i, j] = iv;
+		}
+		public RectGridMap(int height, int width, Func<TValue> getIV)
+		{
+			a = new TValue[height, width];
+			for (int i = 0; i < height; ++i)
+				for (int j = 0; j < width; ++j)
+					a[i, j] = getIV();
+		}
+		public override TValue this[Point key] { get => a[key.i, key.j]; set => a[key.i, key.j] = value; }
+		public override TValue this[int i, int j] { get => a[i, j]; set => a[i, j] = value; }
+	}
+
 	public struct Edge
 	{
-		public static Edge Invalid { get; } = new Edge(-1, -1, long.MinValue);
+		public static Edge Invalid { get; } = new Edge((-1, -1), (-1, -1), long.MinValue);
 
-		public int From { get; }
-		public int To { get; }
+		public Point From { get; }
+		public Point To { get; }
 		public long Cost { get; }
 
-		public Edge(int from, int to, long cost = 1) { From = from; To = to; Cost = cost; }
-		public void Deconstruct(out int from, out int to) { from = From; to = To; }
-		public void Deconstruct(out int from, out int to, out long cost) { from = From; to = To; cost = Cost; }
-		public override string ToString() => $"{From} {To} {Cost}";
+		public Edge(Point from, Point to, long cost = 1) { From = from; To = to; Cost = cost; }
+		public void Deconstruct(out Point from, out Point to) { from = From; to = To; }
+		public void Deconstruct(out Point from, out Point to, out long cost) { from = From; to = To; cost = Cost; }
+		public override string ToString() => $"{{{From}}} {{{To}}} {Cost}";
 
-		public static implicit operator Edge(int[] e) => new Edge(e[0], e[1], e.Length > 2 ? e[2] : 1);
-		public static implicit operator Edge(long[] e) => new Edge((int)e[0], (int)e[1], e.Length > 2 ? e[2] : 1);
-		public static implicit operator Edge((int from, int to) v) => new Edge(v.from, v.to);
-		public static implicit operator Edge((int from, int to, long cost) v) => new Edge(v.from, v.to, v.cost);
+		public static implicit operator Edge((Point from, Point to) v) => new Edge(v.from, v.to);
+		public static implicit operator Edge((Point from, Point to, long cost) v) => new Edge(v.from, v.to, v.cost);
 
 		public Edge Reverse() => new Edge(To, From, Cost);
 	}
 
 	public class CostResult
 	{
-		protected const int InvalidVertex = -1;
+		protected static readonly Point InvalidVertex = (-1, -1);
 
-		public long[] RawCosts { get; }
-		public CostResult(long[] costs) { RawCosts = costs; }
+		public GridMap<long> RawCosts { get; }
+		public CostResult(GridMap<long> costs) { RawCosts = costs; }
 
-		public long this[int vertex] => RawCosts[vertex];
-		public bool IsConnected(int vertex) => RawCosts[vertex] != long.MaxValue;
-		public long GetCost(int vertex, long invalid = -1) => IsConnected(vertex) ? RawCosts[vertex] : invalid;
+		public long this[Point vertex] => RawCosts[vertex];
+		public bool IsConnected(Point vertex) => RawCosts[vertex] != long.MaxValue;
+		public long GetCost(Point vertex, long invalid = -1) => IsConnected(vertex) ? RawCosts[vertex] : invalid;
 	}
 
 	public class UnweightedResult : CostResult
 	{
-		public int[] RawInVertexes { get; }
+		public GridMap<Point> RawInVertexes { get; }
 
-		public UnweightedResult(long[] costs, int[] inVertexes) : base(costs)
+		public UnweightedResult(GridMap<long> costs, GridMap<Point> inVertexes) : base(costs)
 		{
 			RawInVertexes = inVertexes;
 		}
 
-		public int[] GetPathVertexes(int endVertex)
+		public Point[] GetPathVertexes(Point endVertex)
 		{
-			var path = new Stack<int>();
+			var path = new Stack<Point>();
 			for (var v = endVertex; v != InvalidVertex; v = RawInVertexes[v])
 				path.Push(v);
 			return path.ToArray();
 		}
 
-		public Edge[] GetPathEdges(int endVertex)
+		public Edge[] GetPathEdges(Point endVertex)
 		{
 			var path = new Stack<Edge>();
 			for (var v = endVertex; RawInVertexes[v] != InvalidVertex; v = RawInVertexes[v])
@@ -64,22 +104,22 @@ namespace AlgorithmLab.Graphs.Grid
 
 	public class WeightedResult : CostResult
 	{
-		public Edge[] RawInEdges { get; }
+		public GridMap<Edge> RawInEdges { get; }
 
-		public WeightedResult(long[] costs, Edge[] inEdges) : base(costs)
+		public WeightedResult(GridMap<long> costs, GridMap<Edge> inEdges) : base(costs)
 		{
 			RawInEdges = inEdges;
 		}
 
-		public int[] GetPathVertexes(int endVertex)
+		public Point[] GetPathVertexes(Point endVertex)
 		{
-			var path = new Stack<int>();
+			var path = new Stack<Point>();
 			for (var v = endVertex; v != InvalidVertex; v = RawInEdges[v].From)
 				path.Push(v);
 			return path.ToArray();
 		}
 
-		public Edge[] GetPathEdges(int endVertex)
+		public Edge[] GetPathEdges(Point endVertex)
 		{
 			var path = new Stack<Edge>();
 			for (var e = RawInEdges[endVertex]; e.From != InvalidVertex; e = RawInEdges[e.From])
--- Bang/AlgorithmLab/Graphs/Grid/GraphConvert.cs
+++ Bang/AlgorithmLab/Graphs/Grid/GraphConvert.cs
@@ -5,7 +5,7 @@ namespace AlgorithmLab.Graphs.Grid
 {
 	public static class GraphConvert
 	{
-		public static void UnweightedEdgesToMap(List<int>[] map, Edge[] edges, bool directed)
+		public static void UnweightedEdgesToMap(GridMap<List<Point>> map, Edge[] edges, bool directed)
 		{
 			foreach (var e in edges)
 			{
@@ -14,60 +14,13 @@ namespace AlgorithmLab.Graphs.Grid
 			}
 		}
 
-		public static void UnweightedEdgesToMap(List<int>[] map, int[][] edges, bool directed)
-		{
-			foreach (var e in edges)
-			{
-				map[e[0]].Add(e[1]);
-				if (!directed) map[e[1]].Add(e[0]);
-			}
-		}
-
-		public static void WeightedEdgesToMap(List<Edge>[] map, Edge[] edges, bool directed)
+		public static void WeightedEdgesToMap(GridMap<List<Edge>> map, Edge[] edges, bool directed)
 		{
 			foreach (var e in edges)
 			{
 				map[e.From].Add(e);
 				if (!directed) map[e.To].Add(e.Reverse());
 			}
-		}
-
-		public static void WeightedEdgesToMap(List<Edge>[] map, int[][] edges, bool directed)
-		{
-			foreach (var e0 in edges)
-			{
-				Edge e = e0;
-				map[e.From].Add(e);
-				if (!directed) map[e.To].Add(e.Reverse());
-			}
-		}
-
-		public static List<int>[] UnweightedEdgesToMap(int vertexesCount, Edge[] edges, bool directed)
-		{
-			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
-			UnweightedEdgesToMap(map, edges, directed);
-			return map;
-		}
-
-		public static List<int>[] UnweightedEdgesToMap(int vertexesCount, int[][] edges, bool directed)
-		{
-			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<int>());
-			UnweightedEdgesToMap(map, edges, directed);
-			return map;
-		}
-
-		public static List<Edge>[] WeightedEdgesToMap(int vertexesCount, Edge[] edges, bool directed)
-		{
-			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<Edge>());
-			WeightedEdgesToMap(map, edges, directed);
-			return map;
-		}
-
-		public static List<Edge>[] WeightedEdgesToMap(int vertexesCount, int[][] edges, bool directed)
-		{
-			var map = Array.ConvertAll(new bool[vertexesCount], _ => new List<Edge>());
-			WeightedEdgesToMap(map, edges, directed);
-			return map;
 		}
 	}
 }

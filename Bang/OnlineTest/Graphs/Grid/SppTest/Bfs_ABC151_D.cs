f4dccd62846895d6aba9a19927d3a2d5561e6686
Keiho Sakapon <git@saka-pon.net>
2021-01-16 11:57:52 +0900

    Add test

---
+++ Bang/OnlineTest/Graphs/Grid/SppTest/Bfs_ABC151_D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Graphs.Grid;
+
+namespace OnlineTest.Graphs.Grid.SppTest
+{
+	// Test: https://atcoder.jp/contests/abc151/tasks/abc151_d
+	class Bfs_ABC151_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main()
+		{
+			var (h, w) = Read2();
+			var s = GraphConsole.ReadEnclosedGrid(ref h, ref w);
+
+			var M = 0L;
+			for (int i = 0; i < h; i++)
+				for (int j = 0; j < w; j++)
+				{
+					if (s[i, j] == '#') continue;
+
+					var r = ShortestPathCore.Bfs(h, w,
+						v => Array.FindAll(v.Nexts(), nv => s[nv] != '#'),
+						(i, j), (-1, -1));
+
+					for (int x = 0; x < h; x++)
+						for (int y = 0; y < w; y++)
+						{
+							if (s[x, y] == '#') continue;
+
+							M = Math.Max(M, r[(x, y)]);
+						}
+				}
+			Console.WriteLine(M);
+		}
+	}
+}
---
+++ Bang/OnlineTest/Graphs/Grid/SppTest/Bfs_ABC184_E.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Graphs.Grid;
+
+namespace OnlineTest.Graphs.Grid.SppTest
+{
+	// Test: https://atcoder.jp/contests/abc184/tasks/abc184_e
+	class Bfs_ABC184_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main()
+		{
+			var (h, w) = Read2();
+			var s = GraphConsole.ReadEnclosedGrid(ref h, ref w);
+
+			var sv = GridHelper.FindValue(s, 'S');
+			var ev = GridHelper.FindValue(s, 'G');
+
+			var map = new MultiMap<char, Point>();
+			for (int i = 0; i < h; i++)
+				for (int j = 0; j < w; j++)
+				{
+					var c = s[i, j];
+					if (!char.IsLower(c)) continue;
+					map.Add(c, new Point(i, j));
+				}
+
+			// 01-BFS でも AC。ただし、Edge<T>[] を静的に構築する方法では TLE。
+			var r = ShortestPathCore.Bfs(h, w,
+				v =>
+				{
+					var nvs = Array.FindAll(v.Nexts(), nv => s[nv] != '#');
+					var c = s[v];
+					if (!map.ContainsKey(c)) return nvs;
+
+					var nvl = new List<Point>(nvs);
+					nvl.AddRange(map[c]);
+					map.Remove(c);
+					return nvl.ToArray();
+				},
+				sv, ev);
+			Console.WriteLine(r.GetCost(ev));
+		}
+	}
+
+	class MultiMap<TK, TV> : Dictionary<TK, List<TV>>
+	{
+		static List<TV> empty = new List<TV>();
+
+		public void Add(TK key, TV v)
+		{
+			if (ContainsKey(key)) this[key].Add(v);
+			else this[key] = new List<TV> { v };
+		}
+
+		public List<TV> ReadValues(TK key) => ContainsKey(key) ? this[key] : empty;
+	}
+}

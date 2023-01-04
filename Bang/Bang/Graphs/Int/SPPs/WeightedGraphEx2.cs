7594f52b3a3cb5963f939050cbba45ee881aecf2
Keiho Sakapon <git@saka-pon.net>
2023-01-04 19:35:30 +0900

    Refactor

--- Bang/Bang/Graphs/Int/SPPs/WeightedGraphEx2.cs
+++ Bang/Bang/Graphs/Int/SPPs/WeightedGraphEx2.cs
@@ -5,16 +5,16 @@ namespace Bang.Graphs.Int.SPPs
 {
 	public static class WeightedGraphEx2
 	{
-		static Vertex[] CreateVertexes(int n)
+		static Vertex[] CreateVertexes(this WeightedGraph graph)
 		{
-			var vs = new Vertex[n];
-			for (int v = 0; v < n; ++v) vs[v] = new Vertex(v);
+			var vs = new Vertex[graph.VertexesCount];
+			for (int v = 0; v < vs.Length; ++v) vs[v] = new Vertex(v);
 			return vs;
 		}
 
 		public static Vertex[] DijkstraTree(this WeightedGraph graph, int sv, int ev = -1)
 		{
-			var vs = CreateVertexes(graph.VertexesCount);
+			var vs = graph.CreateVertexes();
 			vs[sv].Cost = 0;
 			var q = new SortedSet<(long, int)> { (0, sv) };
 
@@ -42,7 +42,7 @@ namespace Bang.Graphs.Int.SPPs
 		// Dijkstra 法の特別な場合です。
 		public static Vertex[] ModBFSTree(this WeightedGraph graph, int mod, int sv, int ev = -1)
 		{
-			var vs = CreateVertexes(graph.VertexesCount);
+			var vs = graph.CreateVertexes();
 			vs[sv].Cost = 0;
 			var qs = Array.ConvertAll(new bool[mod], _ => new Queue<int>());
 			qs[0].Enqueue(sv);

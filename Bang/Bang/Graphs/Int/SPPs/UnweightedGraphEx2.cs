2fc26956b33a08aaac4c45e41c304b7942b5e3ed
Keiho Sakapon <git@saka-pon.net>
2023-01-04 16:38:15 +0900

    Add UnweightedGraph class

---
+++ Bang/Bang/Graphs/Int/SPPs/UnweightedGraphEx2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bang.Graphs.Int.SPPs
+{
+	[System.Diagnostics.DebuggerDisplay(@"\{{Id}: Cost = {Cost}\}")]
+	public class Vertex
+	{
+		public int Id { get; }
+		public long Cost { get; set; } = long.MaxValue;
+		public bool IsConnected => Cost != long.MaxValue;
+		public Vertex Parent { get; set; }
+		public Vertex(int id) { Id = id; }
+
+		public int[] GetPathVertexes()
+		{
+			var path = new Stack<int>();
+			for (var v = this; v != null; v = v.Parent)
+				path.Push(v.Id);
+			return path.ToArray();
+		}
+
+		public (int, int)[] GetPathEdges()
+		{
+			var path = new Stack<(int, int)>();
+			for (var v = this; v.Parent != null; v = v.Parent)
+				path.Push((v.Parent.Id, v.Id));
+			return path.ToArray();
+		}
+	}
+
+	public static class UnweightedGraphEx2
+	{
+		static Vertex[] CreateVertexes(this UnweightedGraph graph)
+		{
+			var vs = new Vertex[graph.VertexesCount];
+			for (int v = 0; v < vs.Length; ++v) vs[v] = new Vertex(v);
+			return vs;
+		}
+
+		// 最短経路とは限りません。
+		// 連結性のみを判定する場合は、DFS、BFS または Union-Find を利用します。
+		public static Vertex[] ConnectivityByDFS(this UnweightedGraph graph, int sv, int ev = -1)
+		{
+			var vs = graph.CreateVertexes();
+			vs[sv].Cost = 0;
+			DFS(sv);
+			return vs;
+
+			bool DFS(int v)
+			{
+				if (v == ev) return true;
+				var vo = vs[v];
+
+				foreach (var nv in graph.GetEdges(v))
+				{
+					var nvo = vs[nv];
+					if (nvo.Cost != long.MaxValue) continue;
+					nvo.Cost = vo.Cost + 1;
+					nvo.Parent = vo;
+					if (DFS(nv)) return true;
+				}
+				return false;
+			}
+		}
+
+		public static Vertex[] ShortestByBFS(this UnweightedGraph graph, int sv, int ev = -1)
+		{
+			var vs = graph.CreateVertexes();
+			vs[sv].Cost = 0;
+			var q = new Queue<int>();
+			q.Enqueue(sv);
+
+			while (q.Count > 0)
+			{
+				var v = q.Dequeue();
+				if (v == ev) return vs;
+				var vo = vs[v];
+				var nc = vo.Cost + 1;
+
+				foreach (var nv in graph.GetEdges(v))
+				{
+					var nvo = vs[nv];
+					if (nvo.Cost <= nc) continue;
+					nvo.Cost = nc;
+					nvo.Parent = vo;
+					q.Enqueue(nv);
+				}
+			}
+			return vs;
+		}
+	}
+}
---
+++ Bang/Bang/Graphs/Int/UnweightedGraph.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 頂点 id は整数 [0, n) とします。
+// GetEdges を抽象化します。
+// 実行結果は入力グラフから分離されます。
+namespace Bang.Graphs.Int
+{
+	[System.Diagnostics.DebuggerDisplay(@"VertexesCount = {VertexesCount}")]
+	public abstract class UnweightedGraph
+	{
+		protected readonly int n;
+		public int VertexesCount => n;
+		public abstract List<int> GetEdges(int v);
+		protected UnweightedGraph(int n) { this.n = n; }
+	}
+
+	public class ListUnweightedGraph : UnweightedGraph
+	{
+		protected readonly List<int>[] map;
+		public List<int>[] AdjacencyList => map;
+		public override List<int> GetEdges(int v) => map[v];
+
+		public ListUnweightedGraph(List<int>[] map) : base(map.Length) { this.map = map; }
+		public ListUnweightedGraph(int n) : base(n)
+		{
+			map = Array.ConvertAll(new bool[n], _ => new List<int>());
+		}
+		public ListUnweightedGraph(int n, IEnumerable<(int from, int to)> edges, bool twoWay) : this(n)
+		{
+			foreach (var (from, to) in edges) AddEdge(from, to, twoWay);
+		}
+
+		public void AddEdge(int from, int to, bool twoWay)
+		{
+			map[from].Add(to);
+			if (twoWay) map[to].Add(from);
+		}
+	}
+
+	public class UnweightedGrid : UnweightedGraph
+	{
+		protected readonly int h, w;
+		public int Height => h;
+		public int Width => w;
+		public UnweightedGrid(int h, int w) : base(h * w) { this.h = h; this.w = w; }
+
+		public int ToVertexId(int i, int j) => w * i + j;
+		public (int i, int j) FromVertexId(int v) => (v / w, v % w);
+
+		public static (int di, int dj)[] NextsDelta { get; } = new[] { (0, -1), (0, 1), (-1, 0), (1, 0) };
+		public static (int di, int dj)[] NextsDelta8 { get; } = new[] { (0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1) };
+
+		public override List<int> GetEdges(int v)
+		{
+			var (i, j) = (v / w, v % w);
+			var l = new List<int>();
+			foreach (var (di, dj) in NextsDelta)
+			{
+				var (ni, nj) = (i + di, j + dj);
+				if (0 <= ni && ni < h && 0 <= nj && nj < w) l.Add(w * ni + nj);
+			}
+			return l;
+		}
+	}
+
+	public class CharUnweightedGrid : UnweightedGrid
+	{
+		readonly char[][] s;
+		readonly char wall;
+		public char[][] Cells => s;
+		public char[] this[int i] => s[i];
+		public CharUnweightedGrid(char[][] s, char wall = '#') : base(s.Length, s[0].Length) { this.s = s; this.wall = wall; }
+		public CharUnweightedGrid(string[] s, char wall = '#') : this(ToArrays(s), wall) { }
+
+		public static char[][] ToArrays(string[] s) => Array.ConvertAll(s, l => l.ToCharArray());
+
+		public (int i, int j) FindCell(char c)
+		{
+			for (int i = 0; i < h; ++i)
+				for (int j = 0; j < w; ++j)
+					if (s[i][j] == c) return (i, j);
+			return (-1, -1);
+		}
+
+		public int FindVertexId(char c)
+		{
+			for (int i = 0; i < h; ++i)
+				for (int j = 0; j < w; ++j)
+					if (s[i][j] == c) return w * i + j;
+			return -1;
+		}
+
+		public override List<int> GetEdges(int v)
+		{
+			var (i, j) = (v / w, v % w);
+			var l = new List<int>();
+			foreach (var (di, dj) in NextsDelta)
+			{
+				var (ni, nj) = (i + di, j + dj);
+				if (0 <= ni && ni < h && 0 <= nj && nj < w && s[ni][nj] != wall) l.Add(w * ni + nj);
+			}
+			return l;
+		}
+	}
+}

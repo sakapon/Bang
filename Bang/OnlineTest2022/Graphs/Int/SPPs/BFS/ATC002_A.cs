c6db2872ae7ec327001ed76f0b61f9c92bf2acf9
Keiho Sakapon <git@saka-pon.net>
2023-01-04 22:15:38 +0900

    Add test

---
+++ Bang/OnlineTest2022/Graphs/Int/SPPs/BFS/ATC002_A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bang.Graphs.Int;
+using Bang.Graphs.Int.SPPs;
+
+namespace OnlineTest2022.Graphs.Int.SPPs.BFS
+{
+	// Test: https://atcoder.jp/contests/atc002/tasks/abc007_3
+	class ATC002_A
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (h, w) = Read2();
+			var (si, sj) = Read2();
+			var (ei, ej) = Read2();
+			var s = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
+
+			var grid = new CharUnweightedGrid(s);
+
+			si--; sj--;
+			ei--; ej--;
+			var sv = grid.ToVertexId(si, sj);
+			var ev = grid.ToVertexId(ei, ej);
+
+			var r = grid.ShortestByBFS(sv, ev);
+			return r[ev];
+		}
+	}
+}
---
+++ Bang/OnlineTest2022/Graphs/Int/SPPs/DFS/ATC001_A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bang.Graphs.Int;
+using Bang.Graphs.Int.SPPs;
+
+namespace OnlineTest2022.Graphs.Int.SPPs.DFS
+{
+	// Test: https://atcoder.jp/contests/atc001/tasks/dfs_a
+	class ATC001_A
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+		static bool Solve()
+		{
+			var (h, w) = Read2();
+			var s = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
+
+			var grid = new CharUnweightedGrid(s);
+			var sv = grid.FindVertexId('s');
+			var ev = grid.FindVertexId('g');
+
+			var r = grid.ConnectivityByDFS(sv, ev);
+			return r[ev];
+		}
+	}
+}

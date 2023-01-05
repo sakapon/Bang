174bd02a7d42834be4b18e3a57ebb5cd16bdccd5
Keiho Sakapon <git@saka-pon.net>
2023-01-05 17:46:44 +0900

    Add PathResult<T> class

---
+++ Bang/Bang/Graphs/Typed/SPPs/PathResult.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bang.Graphs.Typed.SPPs
+{
+	[System.Diagnostics.DebuggerDisplay(@"VertexesCount = {VertexesCount}")]
+	public class PathResult<T>
+	{
+		[System.Diagnostics.DebuggerDisplay(@"\{{Id}: Cost = {Cost}\}")]
+		public class Vertex
+		{
+			public T Id { get; }
+			public long Cost { get; set; } = long.MaxValue;
+			public bool IsConnected => Cost != long.MaxValue;
+			public Vertex Parent { get; set; }
+			public Vertex(T id) { Id = id; }
+
+			public T[] GetPathVertexes()
+			{
+				var path = new Stack<T>();
+				for (var v = this; v != null; v = v.Parent)
+					path.Push(v.Id);
+				return path.ToArray();
+			}
+
+			public (T, T)[] GetPathEdges()
+			{
+				var path = new Stack<(T, T)>();
+				for (var v = this; v.Parent != null; v = v.Parent)
+					path.Push((v.Parent.Id, v.Id));
+				return path.ToArray();
+			}
+		}
+
+		public Dictionary<T, Vertex> Vertexes { get; }
+		public int VertexesCount => Vertexes.Count;
+		public Vertex this[T v] => Vertexes[v];
+		public PathResult(Dictionary<T, Vertex> vertexes) { Vertexes = vertexes; }
+		public PathResult(IEnumerable<T> vertexes) : this(CreateVertexes(vertexes)) { }
+
+		static Dictionary<T, Vertex> CreateVertexes(IEnumerable<T> vertexes)
+		{
+			var vs = new Dictionary<T, Vertex>();
+			foreach (var v in vertexes) vs[v] = new Vertex(v);
+			return vs;
+		}
+	}
+}
--- Bang/Bang/Graphs/Typed/SPPs/UnweightedPath2.cs
+++ Bang/Bang/Graphs/Typed/SPPs/UnweightedPath2.cs
@@ -1,53 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 
-// GetEdges を抽象化します。
-// 実行結果は入力グラフから分離されます。
 namespace Bang.Graphs.Typed.SPPs
 {
-	[System.Diagnostics.DebuggerDisplay(@"\{{Id}: Cost = {Cost}\}")]
-	public class Vertex<T>
+	public static class UnweightedPath2
 	{
-		public T Id { get; }
-		public long Cost { get; set; } = long.MaxValue;
-		public bool IsConnected => Cost != long.MaxValue;
-		public Vertex<T> Parent { get; set; }
-		public Vertex(T id) { Id = id; }
-
-		public T[] GetPathVertexes()
-		{
-			var path = new Stack<T>();
-			for (var v = this; v != null; v = v.Parent)
-				path.Push(v.Id);
-			return path.ToArray();
-		}
-
-		public (T, T)[] GetPathEdges()
-		{
-			var path = new Stack<(T, T)>();
-			for (var v = this; v.Parent != null; v = v.Parent)
-				path.Push((v.Parent.Id, v.Id));
-			return path.ToArray();
-		}
-	}
-
-	public static class UnweightedGraphEx
-	{
-		static Dictionary<T, Vertex<T>> CreateVertexes<T>(this UnweightedGraph<T> graph)
-		{
-			var vs = new Dictionary<T, Vertex<T>>();
-			foreach (var v in graph.GetVertexes()) vs[v] = new Vertex<T>(v);
-			return vs;
-		}
-
 		// 最短経路とは限りません。
 		// 連結性のみを判定する場合は、DFS、BFS または Union-Find を利用します。
-		public static Dictionary<T, Vertex<T>> ConnectivityByDFS<T>(this UnweightedGraph<T> graph, T sv, T ev)
+		public static PathResult<T> DFSTree<T>(this UnweightedGraph<T> graph, T sv, T ev)
 		{
-			var vs = graph.CreateVertexes();
+			var r = new PathResult<T>(graph.GetVertexes());
+			var vs = r.Vertexes;
 			vs[sv].Cost = 0;
 			DFS(sv);
-			return vs;
+			return r;
 
 			bool DFS(T v)
 			{
@@ -66,9 +32,10 @@ namespace Bang.Graphs.Typed.SPPs
 			}
 		}
 
-		public static Dictionary<T, Vertex<T>> ShortestByBFS<T>(this UnweightedGraph<T> graph, T sv, T ev)
+		public static PathResult<T> BFSTree<T>(this UnweightedGraph<T> graph, T sv, T ev)
 		{
-			var vs = graph.CreateVertexes();
+			var r = new PathResult<T>(graph.GetVertexes());
+			var vs = r.Vertexes;
 			vs[sv].Cost = 0;
 			var q = new Queue<T>();
 			q.Enqueue(sv);
@@ -76,7 +43,7 @@ namespace Bang.Graphs.Typed.SPPs
 			while (q.Count > 0)
 			{
 				var v = q.Dequeue();
-				if (vs.Comparer.Equals(v, ev)) return vs;
+				if (vs.Comparer.Equals(v, ev)) return r;
 				var vo = vs[v];
 				var nc = vo.Cost + 1;
 
@@ -89,7 +56,7 @@ namespace Bang.Graphs.Typed.SPPs
 					q.Enqueue(nv);
 				}
 			}
-			return vs;
+			return r;
 		}
 	}
 }

78eebf6db820119f27aece37e6d745b27ccb3fc3
Keiho Sakapon <git@saka-pon.net>
2023-01-05 19:51:50 +0900

    Add WeightedGraph<T> class

---
+++ Bang/Bang/Graphs/Typed/SPPs/WeightedPath1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bang.Graphs.Typed.SPPs
+{
+	public static class WeightedPath1
+	{
+		static Dictionary<T, TValue> CreateVertexes<T, TValue>(this WeightedGraph<T> graph, TValue value)
+		{
+			var d = new Dictionary<T, TValue>();
+			foreach (var v in graph.GetVertexes()) d[v] = value;
+			return d;
+		}
+
+		public static Dictionary<T, long> Dijkstra<T>(this WeightedGraph<T> graph, T sv, T ev)
+		{
+			var costs = graph.CreateVertexes(long.MaxValue);
+			costs[sv] = 0;
+			var q = new SortedSet<(long, T)> { (0, sv) };
+
+			while (q.Count > 0)
+			{
+				var (c, v) = q.Min;
+				q.Remove((c, v));
+				if (costs.Comparer.Equals(v, ev)) return costs;
+
+				foreach (var (nv, cost) in graph.GetEdges(v))
+				{
+					var nc = c + cost;
+					if (costs[nv] <= nc) continue;
+					if (costs[nv] != long.MaxValue) q.Remove((costs[nv], nv));
+					q.Add((nc, nv));
+					costs[nv] = nc;
+				}
+			}
+			return costs;
+		}
+
+		// Dijkstra 法の特別な場合です。
+		public static Dictionary<T, long> ShortestByModBFS<T>(this WeightedGraph<T> graph, int mod, T sv, T ev)
+		{
+			var costs = graph.CreateVertexes(long.MaxValue);
+			costs[sv] = 0;
+			var qs = Array.ConvertAll(new bool[mod], _ => new Queue<T>());
+			qs[0].Enqueue(sv);
+			var qc = 1;
+
+			for (long c = 0; qc > 0; ++c)
+			{
+				var q = qs[c % mod];
+				while (q.Count > 0)
+				{
+					var v = q.Dequeue();
+					--qc;
+					if (costs.Comparer.Equals(v, ev)) return costs;
+					if (costs[v] < c) continue;
+
+					foreach (var (nv, cost) in graph.GetEdges(v))
+					{
+						var nc = c + cost;
+						if (costs[nv] <= nc) continue;
+						costs[nv] = nc;
+						qs[nc % mod].Enqueue(nv);
+						++qc;
+					}
+				}
+			}
+			return costs;
+		}
+	}
+}
---
+++ Bang/Bang/Graphs/Typed/WeightedGraph.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+// GetEdges を抽象化します。
+// 実行結果は入力グラフから分離されます。
+namespace Bang.Graphs.Typed
+{
+	[System.Diagnostics.DebuggerDisplay(@"VertexesCount = {VertexesCount}")]
+	public abstract class WeightedGraph<T>
+	{
+		public abstract int VertexesCount { get; }
+		public abstract IEnumerable<T> GetVertexes();
+		public abstract List<(T to, long cost)> GetEdges(T v);
+	}
+
+	public class ListWeightedGraph<T> : WeightedGraph<T>
+	{
+		protected readonly Dictionary<T, List<(T to, long cost)>> map;
+		public Dictionary<T, List<(T to, long cost)>> AdjacencyList => map;
+		public override int VertexesCount => map.Count;
+		public override IEnumerable<T> GetVertexes() => map.Keys;
+		public override List<(T to, long cost)> GetEdges(T v) => map[v];
+
+		public ListWeightedGraph(Dictionary<T, List<(T to, long cost)>> map) { this.map = map ?? new Dictionary<T, List<(T to, long cost)>>(); }
+		public ListWeightedGraph() : this(null) { }
+		public ListWeightedGraph(IEnumerable<(T from, T to, int cost)> edges, bool twoWay) : this()
+		{
+			foreach (var (from, to, cost) in edges) AddEdge(from, to, twoWay, cost);
+		}
+		public ListWeightedGraph(IEnumerable<(T from, T to, long cost)> edges, bool twoWay) : this()
+		{
+			foreach (var (from, to, cost) in edges) AddEdge(from, to, twoWay, cost);
+		}
+
+		public bool AddVertex(T v)
+		{
+			if (map.ContainsKey(v)) return false;
+			map[v] = new List<(T to, long cost)>();
+			return true;
+		}
+
+		public void AddEdge(T from, T to, bool twoWay, long cost)
+		{
+			if (!map.TryGetValue(from, out var edges)) map[from] = edges = new List<(T to, long cost)>();
+			edges.Add((to, cost));
+			if (twoWay)
+			{
+				if (!map.TryGetValue(to, out edges)) map[to] = edges = new List<(T to, long cost)>();
+				edges.Add((from, cost));
+			}
+		}
+	}
+}

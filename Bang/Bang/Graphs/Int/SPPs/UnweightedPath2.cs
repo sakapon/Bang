911de87bc6774f6c96bf900efe2084bd266906db
Keiho Sakapon <git@saka-pon.net>
2023-01-05 10:21:45 +0900

    Separate classes

--- Bang/Bang/Graphs/Int/SPPs/UnweightedPath2.cs
+++ Bang/Bang/Graphs/Int/SPPs/UnweightedPath2.cs
@@ -29,16 +29,9 @@ namespace Bang.Graphs.Int.SPPs
 		}
 	}
 
-	public static class PathEx2
+	public static class UnweightedPath2
 	{
 		static Vertex[] CreateVertexes(this UnweightedGraph graph)
-		{
-			var vs = new Vertex[graph.VertexesCount];
-			for (int v = 0; v < vs.Length; ++v) vs[v] = new Vertex(v);
-			return vs;
-		}
-
-		static Vertex[] CreateVertexes(this WeightedGraph graph)
 		{
 			var vs = new Vertex[graph.VertexesCount];
 			for (int v = 0; v < vs.Length; ++v) vs[v] = new Vertex(v);
@@ -92,68 +85,6 @@ namespace Bang.Graphs.Int.SPPs
 					nvo.Cost = nc;
 					nvo.Parent = vo;
 					q.Enqueue(nv);
-				}
-			}
-			return vs;
-		}
-
-		public static Vertex[] DijkstraTree(this WeightedGraph graph, int sv, int ev = -1)
-		{
-			var vs = graph.CreateVertexes();
-			vs[sv].Cost = 0;
-			var q = new SortedSet<(long, int)> { (0, sv) };
-
-			while (q.Count > 0)
-			{
-				var (c, v) = q.Min;
-				q.Remove((c, v));
-				if (v == ev) return vs;
-				var vo = vs[v];
-
-				foreach (var (nv, cost) in graph.GetEdges(v))
-				{
-					var nvo = vs[nv];
-					var nc = c + cost;
-					if (nvo.Cost <= nc) continue;
-					if (nvo.Cost != long.MaxValue) q.Remove((nvo.Cost, nv));
-					q.Add((nc, nv));
-					nvo.Cost = nc;
-					nvo.Parent = vo;
-				}
-			}
-			return vs;
-		}
-
-		// Dijkstra 法の特別な場合です。
-		public static Vertex[] ModBFSTree(this WeightedGraph graph, int mod, int sv, int ev = -1)
-		{
-			var vs = graph.CreateVertexes();
-			vs[sv].Cost = 0;
-			var qs = Array.ConvertAll(new bool[mod], _ => new Queue<int>());
-			qs[0].Enqueue(sv);
-			var qc = 1;
-
-			for (long c = 0; qc > 0; ++c)
-			{
-				var q = qs[c % mod];
-				while (q.Count > 0)
-				{
-					var v = q.Dequeue();
-					--qc;
-					if (v == ev) return vs;
-					var vo = vs[v];
-					if (vo.Cost < c) continue;
-
-					foreach (var (nv, cost) in graph.GetEdges(v))
-					{
-						var nvo = vs[nv];
-						var nc = c + cost;
-						if (nvo.Cost <= nc) continue;
-						nvo.Cost = nc;
-						nvo.Parent = vo;
-						qs[nc % mod].Enqueue(nv);
-						++qc;
-					}
 				}
 			}
 			return vs;
